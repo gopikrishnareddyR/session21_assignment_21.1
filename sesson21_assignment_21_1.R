e3016203089f390327c0026d63c20c43d8d2c5a9
gopikrishnareddyR <40296528+gopikrishnareddyR@users.noreply.github.com>
2018-10-09 23:53:47 +0530

    Add files via upload

---
+++ sesson21_assignment_21_1.R
@@ -0,0 +1,23 @@
+#session21_assignment_21.1
+
+#1Use the below given data set
+getwd()
+path<-"C:/Users/DELL/Documents"
+
+setwd(path)
+dataset<-read.csv("C:/Users/DELL/Documents/epi_r.csv", sep = ",", header = TRUE)
+
+View(dataset)
+#2.Perform the below given activities:
+
+
+#a.Apply PCA to the dataset and show proportion of variance
+
+
+
+#b.Perform PCA using SVD approach
+
+
+
+
+#c.Show the graphs of PCA components
